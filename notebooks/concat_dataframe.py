462c42d8297851046e9bb2d2c8604990ad0c8d54
Gustavo Santiago Rosa <gudaogustavo@gmail.com>
2026-03-04 20:22:16 -0300

    chore: concatenação de dados e testes
    Feito função para concatenação dos meses e salvando na pasta processed.

---
+++ notebooks/concat_dataframe.py
@@ -0,0 +1,28 @@
+#%%
+
+import pandas as pd
+import glob
+
+arquivos_csv = glob.glob('..\\data\\raw\\*.csv')
+
+ls_dataframes = []
+
+for arquivo in arquivos_csv:
+    df_mes = pd.read_csv(arquivo, sep=";", encoding='latin-1')
+
+    ls_dataframes.append(df_mes)
+
+    df = pd.concat(ls_dataframes, ignore_index=True)
+
+df.shape
+# %%
+#Teste de verificação quantidade de linhas em cada BD
+df_teste = pd.read_csv('..\\data\\raw\\202506_CPGF.csv', sep=";", encoding="latin-1")
+df_teste.shape
+# %%
+
+caminho = ('..\\data\\processed\\CPGF_consolidado.csv')
+
+df.to_csv(caminho, sep=";", encoding='utf-8', index=False)
+
+# %%
